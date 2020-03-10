2b8ef8889ff0ef78fd44e3e54841e2059e645422
鸿玄 <18981914907@189.cn>
2020-03-10 12:24:04 +0800

    版本升级后验证器改变

--- app/Validator/Rule/AlphaDashRule.php
+++ app/Validator/Rule/AlphaDashRule.php
@@ -1,4 +1,12 @@
 <?php declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://swoft.org/docs
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
 
 namespace App\Validator\Rule;
 
@@ -15,16 +23,17 @@ use Swoft\Validator\Exception\ValidatorException;
 class AlphaDashRule implements RuleInterface
 {
     /**
-     * @param array $data
+     * @param array  $data
      * @param string $propertyName
      * @param object $item
-     * @param null $default
+     * @param null   $default
+     * @param bool   $strict
      *
      * @return array
      * @throws ValidatorException
      */
     public function validate(array $data, string $propertyName, $item, $default = null, $strict = false): array
-{
+    {
         $message = $item->getMessage();
         if (!isset($data[$propertyName]) && $default === null) {
             $message = (empty($message)) ? sprintf('%s must exist!', $propertyName) : $message;
@@ -33,11 +42,10 @@ class AlphaDashRule implements RuleInterface
 
         $rule = '/^[A-Za-z0-9\-\_]+$/';
         if (preg_match($rule, $data[$propertyName])) {
-            return [$data];
+            return $data;
         }
 
         $message = (empty($message)) ? sprintf('%s must be a email', $propertyName) : $message;
         throw new ValidatorException($message);
     }
-
 }
